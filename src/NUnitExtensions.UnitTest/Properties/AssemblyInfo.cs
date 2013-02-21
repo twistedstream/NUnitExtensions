e2cdd9ef501747bd2195f8d03493f5cbf348a2d8
Peter Stromquist <peter@twistedstream.com>
2013-02-21 16:01:45 -0600

    Depricate GlobalAssemblyInfo.cs file
    Replace CommonAssemblyInfo.cs with the GlobalAssemblyInfo.cs file auto-generated by the AssemblyInfo task in the Build.proj file.
    Add GlobalAssemblyInfo.cs file to .gitignore since its generated and does not need to be in source control.
    The only side-effect is that when someone initially pulls down the code, they will need to run \build.cmd before they can compile in VS, since it will be looking for the generated file.

--- src/NUnitExtensions.UnitTest/Properties/AssemblyInfo.cs
+++ src/NUnitExtensions.UnitTest/Properties/AssemblyInfo.cs
@@ -3,6 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("TS.NUnitExtensions.Test")]
 [assembly: AssemblyDescription("Unit tests for TS.NUnitExtensions")]
-[assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("358bb65d-100c-4e84-b1bb-aa2b6c697c22")]
--- src/NUnitExtensions/Properties/AssemblyInfo.cs
+++ src/NUnitExtensions/Properties/AssemblyInfo.cs
@@ -3,6 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("TS.NUnitExtensions")]
 [assembly: AssemblyDescription("Helpers for writing unit tests")]
-[assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9AA76836-5867-4D6E-A46C-8F6C1E0F0987")]
