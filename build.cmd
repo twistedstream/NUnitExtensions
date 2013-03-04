2ed4edde2daf573f39928bfc0f88aa6cb739d21c
Peter Stromquist <peter@twistedstream.com>
2013-03-04 00:29:33 -0600

    Add Buildage submodule to build
    Pulling the Buildage scripts from GitHub.
    Updated /build.cmd to match the sample in Buildage which will execute a build target if specified.

--- build.cmd
+++ build.cmd
@@ -1,1 +1,4 @@
-%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\build\Buildage\Build.proj
+ @ECHO OFF
+ SET BuildCmd=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\build\Buildage\Build.proj
+ IF [%1]==[] (%BuildCmd%) ELSE (%BuildCmd% /t:%1)
+ 
