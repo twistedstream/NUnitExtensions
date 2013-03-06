73316d0ab26e67f0d3f9a4cfdcdbdac78bbe6a31
Peter Stromquist <peter@twistedstream.com>
2013-03-06 01:17:53 -0600

    Update to latest Buildage version: f2d75c4d0c

--- src/NUnitExtensions.UnitTest/Properties/AssemblyInfo.cs
+++ src/NUnitExtensions.UnitTest/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitExtensions Unit Tests")]
+[assembly: AssemblyDescription("Unit tests for the types in NUnitExtensions.")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("358bb65d-100c-4e84-b1bb-aa2b6c697c22")]
--- src/NUnitExtensions/Properties/AssemblyInfo.cs
+++ src/NUnitExtensions/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitExtensions")]
+[assembly: AssemblyDescription("Handy base test classes and other extensions useful for TDD using NUnit.")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("9AA76836-5867-4D6E-A46C-8F6C1E0F0987")]
